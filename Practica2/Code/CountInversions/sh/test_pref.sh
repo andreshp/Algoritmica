e8c6de8b0eb28f02606b1994a648e1acc4e12451
isega24 <isega24@hotmail.com>
2015-04-08 20:30:13 +0200

    Differents bad ideas for CountInversions implemented

--- Practica2/Code/CountInversions/sh/test_pref.sh
+++ Practica2/Code/CountInversions/sh/test_pref.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 g++ -o./preferences ./src/preferences.cpp
+g++ -o ./preferencesBuble ./src/preferencesBuble.cpp
+g++ -o ./preferencesFor ./src/preferencesFor.cpp
 
 ./preferences ./test/easytest.txt           #6
 ./preferences ./test/TestCase1.txt          #28
@@ -13,5 +15,27 @@ g++ -o./preferences ./src/preferences.cpp
 ./preferences ./test/INPUT10000.txt         #24970965
 ./preferences ./test/IntegerArray.txt       #2407905288
 
-rm ./preferences
+./preferencesBuble ./test/easytest.txt           #6
+./preferencesBuble ./test/TestCase1.txt          #28
+./preferencesBuble ./test/TestCase2.txt          #110          
+./preferencesBuble ./test/TestCase3.txt          #606
+./preferencesBuble ./test/INPUT500.txt           #62248
+./preferencesBuble ./test/INPUT1000.txt          #261237
+./preferencesBuble ./test/INPUT2000.txt          #1001782
+./preferencesBuble ./test/INPUT5000.txt          #6243161
+./preferencesBuble ./test/INPUT10000.txt         #24970965
+./preferencesBuble ./test/IntegerArray.txt       #2407905288
+
+./preferencesFor ./test/easytest.txt           #6
+./preferencesFor ./test/TestCase1.txt          #28
+./preferencesFor ./test/TestCase2.txt          #110          
+./preferencesFor ./test/TestCase3.txt          #606
+./preferencesFor ./test/INPUT500.txt           #62248
+./preferencesFor ./test/INPUT1000.txt          #261237
+./preferencesFor ./test/INPUT2000.txt          #1001782
+./preferencesFor ./test/INPUT5000.txt          #6243161
+./preferencesFor ./test/INPUT10000.txt         #24970965
+./preferencesFor ./test/IntegerArray.txt       #2407905288
+
+rm ./preferences ./preferencesFor ./preferencesBuble
 
---
+++ Practica2/Code/CountInversions/src/preferencesBuble.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+#define PRINT_INV   //Uncomment this line to print number of inversions.
+#define PRINT_TIME  //Uncomment this line to print elapsed time.
+#define PRINT_TEXT  //Uncomment this line to print text while showing the results.
+
+
+unsigned long long BubleCount(int * v, int begin,unsigned int tam){
+  int i, j;
+  int aux;
+  unsigned long long count = 0;
+  for (i = begin; i < tam -1; i++){
+    for (j = tam -1; j > i; j--){
+      if (v[j] < v[j-1]){
+        aux = v[j];
+        v[j] = v[j-1];
+        v[j-1] = aux;
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+unsigned long long BubleCount(vector<int> v){
+  return BubleCount(&v[0],0,v.size());
+}
+
+
+
+int main(int argc, char *argv[]){
+    if(argc != 2){
+        cerr << "Expected a file name." << endl;
+        exit(-1);
+    }
+    
+    unsigned long long inversions = 0;
+    vector <int> v;
+    int v_i;
+
+    // Time variables
+    struct timeval tv1, tv2;        // gettimeofday() secs-usecs
+    double tv_usecs; 
+
+    //Data reading
+    ifstream in(argv[1]);
+    if(!in){
+        cerr << "Error: cannot open the file." << endl;
+        exit(-1);
+    }
+    
+    while(!in.eof()){
+        if(in >> v_i)
+            v.push_back(v_i);
+    }
+
+    //Algorithm execution.
+    gettimeofday(&tv1,NULL);
+
+    inversions = BubleCount(&v[0],0,v.size());
+
+    gettimeofday(&tv2,NULL);
+
+    cout << v.size();
+
+    //Results.
+    #ifdef PRINT_INV
+        #ifdef PRINT_TEXT
+            cout << "Number of inversions: ";
+        #endif
+        cout << inversions << endl;
+    #endif
+
+    #ifdef PRINT_TIME
+        #ifdef PRINT_TEXT
+            cout << "Elapsed time: ";
+        #endif
+        
+        // Elapsed time.
+        tv_usecs= ((tv2.tv_sec -tv1.tv_sec )*1E6 + (tv2.tv_usec-tv1.tv_usec)) / 1000000.0;
+        cout << tv_usecs << endl;
+    #endif
+
+    return 0;
+}
