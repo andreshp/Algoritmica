c7ff86e0855b7d63c26899a4f51ba33187310202
isega24 <isega24@hotmail.com>
2015-05-10 19:35:24 +0200

    script

--- Practica3/Code/Problema4/ejecutar_programas.sh
+++ Practica3/Code/Problema4/ejecutar_programas.sh
@@ -7,20 +7,9 @@
 # de ordenación dado como parámetro.
 
 # Variables:
-# Variables:
-INICIO_NOD=100
-FIN_NOD=2500
-INCREMENTO_NOD=100
-INICIO_PROB=1
-FIN_PROB=21
-INCREMENTO_PROB=10
 
-
-i=$INICIO_NOD
-j=$INICIO_PROB
-
-PROGRAMA=primeraIdea
-SALIDA=./Datos
+PROGRAMA=terceraIdea
+SALIDA=./Datos/resultados3.txt
 PARAMETRO=./Datos
 MENSAJE_INICIO="Se inicia la ejecución del algoritmo $1:"
 MENSAJE_FINAL="Fin de la ejecución. Se ha creado un fichero con los resultados.\n"
@@ -34,30 +23,29 @@ echo "$MENSAJE_INICIO"
 INICIO_NOD=100
 FIN_NOD=2500
 INCREMENTO_NOD=100
-INICIO_PROB=1
-FIN_PROB=21
-INCREMENTO_PROB=10
+INICIO_PROB=11
+#FIN_PROB=21
+#INCREMENTO_PROB=10
 
 
 i=$INICIO_NOD
 j=$INICIO_PROB
 
-while [ $j -le $FIN_PROB ]
-do
-    echo $FIN_PROB
-    printf "" > $SALIDA/ejecucion$i$j.txt
-    echo Ejecución prob = $j
-    i=$INICIO_NOD
-    while [ $i -le $FIN_NOD ]
-        do
-            printf "" > $SALIDA/ejecucion$i$j.txt
-            echo $SALIDA/ejecucion$i$j.txt
-            echo Ejecución tam = $i
-            echo "`./$PROGRAMA $PARAMETRO/$i$j.txt`" > $SALIDA/ejecucion$i$j.txt
-            i=$((i+$INCREMENTO_NOD))
-        done
-    j=$((j+$INCREMENTO_PROB))
-done
+#while [ $j -le $FIN_PROB ]
+#do
+ #   echo $FIN_PROB
+  #  printf "" >> $SALIDA
+   # printf "Ejecución prob = $j\n" >> $SALIDA
+   # i=$INICIO_NOD
+while [ $i -le $FIN_NOD ]
+    do
+        echo $SALIDA
+        printf "$i " >> $SALIDA
+        echo "`./$PROGRAMA $PARAMETRO/$i$j.txt`" >> $SALIDA
+        i=$((i+$INCREMENTO_NOD))
+    done
+    #j=$((j+$INCREMENTO_PROB))
+#done
 
 # Se elimina el ejecutable:
 rm $PROGRAMA
